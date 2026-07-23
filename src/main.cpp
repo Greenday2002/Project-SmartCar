26697552110b3361459c26dd706e1b6e851c4c52
Greenday2002 <0447422@schoolsnet.act.edu.au>
2026-07-23 11:01:54 +1000

    defining

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,25 @@
 #include <ESP32Servo.h>
 
 vehicle myCar;
+Servo myServo;
 
 #define leftLED 2
 #define rightLED 12
 #define Anticlockwise Contrarotate 
+#define buzzer 33
+#define servoPin 25 
+
+/*
+myCar.Move(Forward, 255):
+myCar.Move(Backward, 255):
+
+myCar.Move(Clockwise, 255):
+myCar.move(Contrarotate, 255)
+myCar.Move(Anticlockwise, 255):
+
+myCar.Move(Move_Left, 255):
+myCar.Move(Move_Right, 255):
+*/
 
 void setup()
 {
@@ -16,18 +31,23 @@ void setup()
  pinMode(rightLED, OUTPUT);
 
  myCar.Init();
+ myServo.attach(servoPin);
+ myServo.write(0);
 }
 
 void loop()
 {
   digitalWrite(leftLED, HIGH);
   digitalWrite(rightLED, LOW);
-  myCar.Move(Move_Left, 255);
+  //myCar.Move(Move_Left, 255);
+  //tone(buzzer, 262);
+
   delay(1000);
 
   digitalWrite(leftLED, LOW);
   digitalWrite(rightLED, HIGH);
-  myCar.Move(Move_Right, 255);
-  delay(1000);
+  //myCar.Move(Move_Right, 255);
+  //noTone(buzzer);
 
+  delay(1000);
 }

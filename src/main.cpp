30649ba0863ccd25ba2cc7a5e4b28ba2edd258c4
Greenday2002 <0447422@schoolsnet.act.edu.au>
2026-08-10 15:27:58 +1000

    acebot book drive course

--- src/main.cpp
+++ src/main.cpp
@@ -24,30 +24,44 @@ myCar.Move(Move_Left, 255):
 myCar.Move(Move_Right, 255):
 */
 
-void setup()
-{
- Serial.begin(115200);
- pinMode(leftLED, OUTPUT);
- pinMode(rightLED, OUTPUT);
-
- myCar.Init();
- myServo.attach(servoPin);
- myServo.write(0);
+void setup(){
+  myCar.Init();
+  myCar.Move(Forward,255);
+  delay(750);
+  myCar.Move(Clockwise,255);
+  delay(750);
+  myCar.Move(Forward,255);
+  delay(750);
+  myCar.Move(Clockwise,255);
+  delay(750);
+  myCar.Move(Forward,255);
+  delay(750);
+  myCar.Move(Contrarotate, 255);
+  delay(750);
+  myCar.Move(Forward,255);
+  delay(750);
+  myCar.Move(Contrarotate, 255);
+  delay(750);
+  myCar.Move(Forward,255);
+  delay(750);
+  myCar.Move(Move_Right,255);
+  delay(750);
+  myCar.Move(Stop,0);
 }
 
 void loop()
 {
-  digitalWrite(leftLED, HIGH);
-  digitalWrite(rightLED, LOW);
+  //digitalWrite(leftLED, HIGH);
+  //digitalWrite(rightLED, LOW);
   //myCar.Move(Move_Left, 255);
   //tone(buzzer, 262);
 
-  delay(1000);
+  //delay(1000);
 
-  digitalWrite(leftLED, LOW);
-  digitalWrite(rightLED, HIGH);
+  //digitalWrite(leftLED, LOW);
+  //digitalWrite(rightLED, HIGH);
   //myCar.Move(Move_Right, 255);
   //noTone(buzzer);
 
-  delay(1000);
+  //delay(1000);
 }
